4deb4b95d12df03e9c9e5d115a8f88b389b15ef7
Craig Lonsdale <craig@fixedpricecarservice.com.au>
2018-06-03 14:09:34 +0800

    Add in a basic particle effect on Trump to be used for shooting

---
+++ Assets/Scripts/Enemies/ObamaDummy/ObamaDummyHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ObamaDummyHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        currentHealth -= amount;
+    }
+}
